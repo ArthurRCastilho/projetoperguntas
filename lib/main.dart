75526f78c8ffd8ec3e0cdf1bd75814bdf119d65a
ArthurLVidal <arthurodri2017@gmail.com>
2024-04-25 15:48:57 -0300

    Adicionando funções aos botões e definindo os textos dos botões através de um for

--- lib/main.dart
+++ lib/main.dart
@@ -17,28 +17,44 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é sua cor favorita?',
-      'Qual seu animal favorito?',
-      'Quantas letras tem seu nome?',
-      'Qual sua matéria favorita?',
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é sua cor favorita?',
+        'respostas': ['Vermelho', 'Azul', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual seu animal favorito?',
+        'respostas': ['Cachorro', 'Gato', 'Tigre', 'Leão']
+      },
+      {
+        'texto': 'Quantas letras tem seu nome?',
+        'respostas': ['Menos que 5', '6 a 8', '9 a 11', 'Mais que 11']
+      },
+      {
+        'texto': 'Qual sua matéria favorita?',
+        'respostas': ['Matematica', 'Portugues', 'Historia', 'Fisica']
+      },
     ];
+
+    List<Widget> respostas = [];
+    for (String textResp
+        in perguntas[_perguntaSelecionada]['respostas'] as List) {
+      respostas.add(Respostas(textResp, _responder));
+    }
 
     return MaterialApp(
       home: Scaffold(
         // Responsavel para dar estrutura a aplicação
         appBar: AppBar(
-          backgroundColor: Color.fromRGBO(51, 51, 255, 0.553),
+          backgroundColor:const Color.fromRGBO(28, 9, 237, 0.605),
           // foregroundColor: Color.fromRGBO(177, 9, 23, 0.635), Cor da letra
-          shadowColor: Color.fromRGBO(13, 150, 132, 0.499),
+          shadowColor:const Color.fromRGBO(0, 255, 123, 0.498),
           title: const Text('Perguntas'),
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Respostas('Resposta 1'),
-            Respostas('Resposta 2'),
-            Respostas('Resposta 3'),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas,
           ],
         ),
       ),
--- lib/respostas.dart
+++ lib/respostas.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 
 class Respostas extends StatelessWidget {
   String resposta = '';
+  final void Function() quandoSelecionado;
 
-  Respostas(this.resposta);
+  Respostas(this.resposta, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      child: Text(resposta),
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: const ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll<Color>(
+              Color.fromARGB(183, 39, 144, 231)),
+        ),
+        onPressed: quandoSelecionado,
+        child: Text(resposta),
+      ),
     );
   }
 }
